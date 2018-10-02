2b520c43d1cbd455098fb74b3cb2c4e7639f29a8
Sandro Sena <sandro340@hotmail.com>
2018-10-02 00:19:56 -0300

    Changes on Index.html and Style.css adding animate.css, main.js and plugins.js

---
+++ main.js
@@ -0,0 +1,33 @@
+//~ Waypoint
+
+ $(".form-lateral--content").waypoint(() => {
+ $(".form-lateral--content").addClass("animated zoomIn");
+}, { offset: '95%' });
+
+$(".main-content").waypoint(() => {
+    $(".main-content").addClass("animated zoomIn");
+   }, { offset: '95%' });
+
+   $(".img-2").waypoint(() => {
+    $(".img-2").addClass("animated fadeInLeftBig");
+   }, { offset: '95%' });
+
+   $(".img-3").waypoint(() => {
+    $(".img-3").addClass("animated zoomIn");
+   }, { offset: '95%' });
+
+   $(".img-4").waypoint(() => {
+    $(".img-4").addClass("animated fadeInRightBig");
+   }, { offset: '95%' });
+
+   $(".img-5").waypoint(() => {
+    $(".img-5").addClass("animated fadeInLeftBig");
+   }, { offset: '95%' });
+
+   $(".img-6").waypoint(() => {
+    $(".img-6").addClass("animated zoomIn");
+   }, { offset: '95%' });
+
+   $(".img-7").waypoint(() => {
+    $(".img-7").addClass("animated fadeInRightBig");
+   }, { offset: '95%' });
