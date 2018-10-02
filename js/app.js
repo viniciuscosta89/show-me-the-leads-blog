02537cfb704e651ea984db9cc9b47f147890b574
Gilberto Papa <Gilbertopapa013@hotmail.com>
2018-10-02 16:32:26 -0300

    function firebase

--- js/app.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyBUi05h6AW2ZjW0f2u1tGqrgUAg6yRLvLs",
-    authDomain: "show-me-the-leads-blog-87279.firebaseapp.com",
-    databaseURL: "https://show-me-the-leads-blog-87279.firebaseio.com",
-    projectId: "show-me-the-leads-blog-87279",
-    storageBucket: "",
-    messagingSenderId: "185684396743"
-  };
-firebase.initializeApp(config);
--- js/real-time-database.js
+++ js/real-time-database.js
@@ -1,30 +1,57 @@
-var usersList = document.getElementById('usersList');
-var nomeInput = document.getElementById('nome');
-var emailInput = document.getElementById('email');
-var empresaInput = document.getElementById('empresa');
-var addButton = document.getElementById('addButton');
 
-// Ao clicar no botão
-addButton.addEventListener('click', function () {
-    create(nomeInpu.value, emailInput.value, empresaInput.value);
-});
+// Initialize Firebase (ADD YOUR OWN DATA)
+var config = {
+    apiKey: "AIzaSyBUi05h6AW2ZjW0f2u1tGqrgUAg6yRLvLs",
+    authDomain: "show-me-the-leads-blog-87279.firebaseapp.com",
+    databaseURL: "https://show-me-the-leads-blog-87279.firebaseio.com",
+    projectId: "show-me-the-leads-blog-87279",
+    storageBucket: "show-me-the-leads-blog-87279.appspot.com",
+    messagingSenderId: "185684396743"
+  };
+  firebase.initializeApp(config);
+  
+  // Reference messages collection
+  var messagesRef = firebase.database().ref('messages');
+  
+  // Listen for form submit
+  document.getElementById('contactForm').addEventListener('submit', submitForm);
+  
+  // Submit form
+  function submitForm(e){
+    e.preventDefault();
 
-// Função para criar um registro no Firebase
-function create(nome,email,empresa) {
-    var data = {
-        nome: nome,
-        email: email,
-        empresa: empresa
-    };
+    var nomeInput = document.getElementById('nome');
+    var emailInput = document.getElementById('email');
+    var empresaInput = document.getElementById('empresa');
+   // var addButton = getElementById('addButton');
 
-    return firebase.database().ref().child('contatos').push(data);
-}
-
-firebase.database().ref('contatos').on('value', function (snapshot) {
-    usersList.innerHTML = '';
-    snapshot.forEach(function (item) {
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode(item.val().name + ': ' + item.val().email+': ' + item.val().empresa));
-        usersList.appendChild(li);
+    // Save message
+    saveMessage(nomeInput, emailInput, empresaInput);
+  
+    // Show alert
+    //document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    //setTimeout(function(){
+   //   document.querySelector('.alert').style.display = 'none';
+   // },3000);
+  
+    // Clear form
+    document.getElementById('contactForm').reset();
+  }
+  
+  // Function to get get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(nomeInput,emailInput,empresaInput){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      nomeInput:nomeInput,
+      emailInput:emailInput,
+      empresaInput:empresaInput,
+    
     });
-});
+  }
